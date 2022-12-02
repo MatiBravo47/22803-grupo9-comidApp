53621c49bbda621134f8b41d2e76820aaf38e5d5
MatiBravo47 <matibravoneron@gmail.com>
2022-12-02 20:57:42 -0300

    add firebase, add bootstrap, add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Grupo 9 - React
-        </p>
+        <Navbar/>
     </div>
   );
 }
---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCoUfMSg2WtnopHDPmyWmkiPm18ezC_BQo",
+    authDomain: "grupo9-apirecetas.firebaseapp.com",
+    projectId: "grupo9-apirecetas",
+    storageBucket: "grupo9-apirecetas.appspot.com",
+    messagingSenderId: "476187288253",
+    appId: "1:476187288253:web:73599c054a8221fbc60dd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
