24dd62a21e6768e6d7adfb4e939be156947bc76e
MatiBravo47 <matibravoneron@gmail.com>
2022-12-04 21:36:19 -0300

    add new components and images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Muestra from './components/Muestra';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Muestra/>
+        <Muestra/>
+        
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
+import './Navbar.css'
 const Navbar = () => {
     return(
         <>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">ComidApp</a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <i class="fa-solid fa-utensils"></i><span className='logo'>ComidApp</span>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -29,7 +32,7 @@ const Navbar = () => {
                     </ul>
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
                     </form>
                 </div>
             </div>
